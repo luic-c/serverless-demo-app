a5ce05c5c226b9d20daeefa142986f1d5d89c1e7
luic <luluchoypooh0@gmail.com>
2020-05-08 18:30:16 +0800

    new test

--- src/config.js
+++ src/config.js
@@ -19,18 +19,18 @@ const dev = {
 const prod = {
   STRIPE_KEY: "YOUR_STRIPE_PROD_PUBLIC_KEY",
   s3: {
-    REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
-    BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
+    REGION: "us-east-2",
+    BUCKET: "serverless-note-demo-bucket"
   },
   apiGateway: {
-    REGION: "YOUR_PROD_API_GATEWAY_REGION",
-    URL: "YOUR_PROD_API_GATEWAY_URL"
+    REGION: "us-east-1",
+    URL: "https://pcig2h27h6.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "YOUR_PROD_COGNITO_REGION",
-    USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
-    APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
-    IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_VBP0LgX05",
+    APP_CLIENT_ID: "3qr47qas5db714rf5egk9hu8uu",
+    IDENTITY_POOL_ID: "us-east-2:adcac127-96c7-4a46-b430-24784b0fe978"
   }
 };
 

47b498977d927bba1062d3d19f46b86c3e39dc55
luic <luluchoypooh0@gmail.com>
2020-05-08 16:09:11 +0800

    testing

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -87,7 +87,6 @@ export default function Home() {
   return (
     <div className="Home">
       {isAuthenticated ? renderNotes() : renderLander()}
-      { isAuthenticated.none.no }
     </div>
   );
 }

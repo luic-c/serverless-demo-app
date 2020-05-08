ba0e83b1b5d781f9cbfe0b2e2b4b6971a8bf9210
luic <luluchoypooh0@gmail.com>
2020-05-09 03:20:25 +0800

    test

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ const prod = {
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://pcig2h27h6.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "https://pcig2h27h6.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-2",
